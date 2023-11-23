539277dbb6f5bb95558c3aa551a05f73b505f903
Oladipo Omotosho <65185499+OladipoOmotosho@users.noreply.github.com>
2023-11-23 21:50:57 +0100

    day 3

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,10 +1,14 @@
 import Card from "./shared/Card";
 import PropTypes from "prop-types";
+import { FaTimes } from "react-icons/fa";
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, handleDelete }) => {
   return (
-    <Card className="card" reverse={true}>
+    <Card className="card">
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display"> {item.text} </div>
     </Card>
   );
