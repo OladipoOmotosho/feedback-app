9665a69fe3e0ae3f71411852d2d5100acaaf064a
Oladipo Omotosho <65185499+OladipoOmotosho@users.noreply.github.com>
2023-11-23 20:55:51 +0100

    day 3

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,8 +1,12 @@
 import FeedbackItem from "./FeedbackItem";
-
+import PropTypes from "prop-types";
 const FeedbackList = ({ feedback }) => {
+  if (!feedback || feedback.length === 0) {
+    return <p>No feedback yet</p>;
+  }
+
   return (
-    <div>
+    <div className="feedback-list">
       {feedback.map((item) => (
         <FeedbackItem key={item.id} item={item} />
       ))}
@@ -10,4 +14,13 @@ const FeedbackList = ({ feedback }) => {
   );
 };
 
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ),
+};
 export default FeedbackList;
