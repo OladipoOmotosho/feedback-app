8aa8d75d05b52f060d912e614c1ea0bcb29dc72e
Oladipo Omotosho <65185499+OladipoOmotosho@users.noreply.github.com>
2023-11-25 10:15:28 +0100

    day 5

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -4,7 +4,21 @@ import Button from "./shared/Button";
 
 const FeedbackForm = () => {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be at least 10 characters long");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
     setText(e.target.value);
   };
   return (
@@ -18,8 +32,11 @@ const FeedbackForm = () => {
             value={text}
             placeholder="Write a review"
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
