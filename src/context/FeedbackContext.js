d6e89e38622b6cad2b601efd16d09413a6d6a4a5
Oladipo Omotosho <65185499+OladipoOmotosho@users.noreply.github.com>
2024-02-05 21:29:22 +0100

    day...

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,29 +1,26 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This is from context 1",
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: "This is from context 2",
-      rating: 6,
-    },
-    {
-      id: 3,
-      text: "This is fromm context 3",
-      rating: 4,
-    },
-  ]);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, [feedback]);
+
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?_sort=id&_order=desc`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
   const addFeedBack = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
